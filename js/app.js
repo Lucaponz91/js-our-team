128591ef3a5247f6535f520431a6fd1767e64877
Lucaponz91 <lucapo1991@msn.com>
2022-08-03 15:54:19 +0200

    add milestone 1

---
+++ js/app.js
@@ -0,0 +1,47 @@
+// Utilizzando i dati forniti, creare un array di oggetti per rappresentare i membri del team. 
+// Ogni membro è caratterizzato dalle seguenti informazioni: nome, ruolo e foto.
+
+// MILESTONE 0:
+// Creare l’array di oggetti con le informazioni fornite.
+
+
+const ourTeam = [
+    {
+        nome : 'Wayne Barnett',
+        ruolo : 'Founder & CEO',
+        pic : 'wayne-barnett-founder-ceo.jpg'
+    },
+    {
+        nome : 'Angela Caroll',
+        ruolo : 'Chief Editor',
+        pic : 'angela-caroll-chief-editor.jpg'
+    },
+    {
+        nome : 'Walter Gordon',
+        ruolo : 'Office Manager',
+        pic : 'walter-gordon-office-manager.jpg'
+    },
+    {
+        nome : 'Angela Lopez',
+        ruolo : 'Social Media Manager',
+        pic : 'angela-lopez-social-media-manager.jpg'
+    },
+    {
+        nome : 'Scott Estrada',
+        ruolo : 'Developer',
+        pic : 'scott-estrada-developer.jpg'
+    },
+    {
+        nome : 'Barbara Ramos',
+        ruolo : 'Graphic Designer',
+        pic : 'barbara-ramos-graphic-designer.jpg'
+    }
+]
+
+// MILESTONE 1:
+// Stampare su console le informazioni di nome, ruolo e la stringa della foto
+
+console.log(ourTeam)
+
+// MILESTONE 2:
+// Stampare le stesse informazioni su DOM sottoforma di stringhe
